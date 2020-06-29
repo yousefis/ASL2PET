47d7106a43fb224e9cd0fe13631873ae0fce9fdf
syousefi <sahar_yousefi@ymail.com>
2020-06-29 10:58:55 +0200

    test file for getting max and min of scans

--- test_file.py
+++ test_file.py
@@ -30,17 +30,19 @@
 # ##################################################
 import numpy as np
 import  SimpleITK as sitk
-from functions.reader.data_reader import _read_data
+from functions.reader.data_reader_hybrid import _read_data
 if __name__=="__main__":
-    data_path="/exports/lkeb-hpc/syousefi/Data/asl_pet/"
+    # data_path="/exports/lkeb-hpc/syousefi/Data/asl_pet/"
     '''read path of the images for train, test, and validation'''
-    _rd = _read_data(data_path)
-    train_data, validation_data, test_data=_rd.read_data_path()
+    data_path_AMUC = "/exports/lkeb-hpc/syousefi/Data/asl_pet/"
+    data_path_LUMC = "/exports/lkeb-hpc/syousefi/Data/asl_pet2/"
+    _rd = _read_data(data_path_AMUC,data_path_LUMC)
+    train_data, validation_data, test_data=_rd.read_data_path(35)
     m_t1=.00000000000000000000001
     M_t1=1
     for d in train_data:
-        # print(d)
-        t1=sitk.GetArrayFromImage(sitk.ReadImage(d['pet']))
+        print(d['asl'])
+        t1=sitk.GetArrayFromImage(sitk.ReadImage(d['asl']))
         mt1= np.min(t1)
         Mt1= np.max(t1)
         if mt1<m_t1:
@@ -49,3 +51,36 @@ if __name__=="__main__":
             M_t1=Mt1
         print('['+str(mt1)+' , '+str(Mt1)+']')
     print('['+str(m_t1)+' , '+str(M_t1)+']')
+
+
+# import tensorflow as tf
+# #
+# x = tf.constant(5.0)
+# with tf.GradientTape() as tape:
+#     tape.watch(x)
+#     y = x**3
+# with tf.GradientTape() as tape:
+#     tape.watch(x)
+#     y += x**1
+# sess=tf.Session()
+# res=sess.run(tape.gradient(y, x))
+# print(res) # -> 75.0
+
+
+# x = tf.constant(5.0)
+# with tf.GradientTape() as t:
+#     t.watch(x)
+#     loss = x**3
+# with tf.GradientTape() as t:
+#     loss += x**5
+# # t.gradient(loss, x)  # Only differentiates other_loss_fn, not loss_fn
+# sess=tf.Session()
+# res=sess.run(t.gradient(loss, x))
+# print(res)
+
+# # The following is equivalent to the above
+# with tf.GradientTape() as t:
+#   loss = loss_fn()
+#   t.reset()
+#   loss += other_loss_fn()
+# t.gradient(loss, ...)  # Only differentiates other_loss_fn, not loss_fn
