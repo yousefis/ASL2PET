d0e1c437c57396ff548271374d18fdbf006b4332
syousefi <sahar_yousefi@ymail.com>
2020-07-02 16:32:47 +0200

    semi-supervised multi-task patch extractor for high resolution images (1*1*1)

--- run_hybrid_multitask_net_hr.py
+++ run_hybrid_multitask_net_hr.py
@@ -13,7 +13,7 @@
 ## Status: {Research}
 ##################################################
 from datetime import datetime
-from functions.network_caller.hybrid_multi_task_translate import net_translate
+from functions.network_caller.hybrid_multi_task_translate_hr import net_translate
 import numpy as np
 import tensorflow as tf
 import os
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     this function calls the translation network
 
     '''
-    no_averages=35
-    config = [1, 3, 1, 3, 1]
+    no_averages=0
+    config = [3, 5, 5, 5, 3]
     np.random.seed(1)
     tf.set_random_seed(1)
 
@@ -45,8 +45,8 @@ if __name__ == '__main__':
     now = datetime.now()
     date_time = now.strftime("%m%d%Y_%H")
 
-    dc12 = net_translate(data_path_AMUC="/exports/lkeb-hpc/syousefi/Data/asl_pet/",
-                         data_path_LUMC="/exports/lkeb-hpc/syousefi/Data/asl_pet2/",
+    dc12 = net_translate(data_path_AMUC="/exports/lkeb-hpc/syousefi/Data/ASL2PET_high_res/AMUC_high_res/",
+                         data_path_LUMC="/exports/lkeb-hpc/syousefi/Data/ASL2PET_high_res/LUMC_high_res/",
                          server_path=server_path, Logs=Logs,
                          config=config)
     dc12.run_net(no_averages)
